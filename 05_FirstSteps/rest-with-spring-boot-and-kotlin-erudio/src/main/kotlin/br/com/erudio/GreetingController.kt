459e65d7871f7bb36797f27c314fb02ced2b232b
leandrocgsi <leandrocgsi@gmail.com>
2022-02-06 17:18:40 -0300

    First Steps

---
+++ 05_FirstSteps/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/GreetingController.kt
@@ -0,0 +1,19 @@
+package br.com.erudio
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController {
+    private val counter: AtomicLong = AtomicLong()
+    @RequestMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String?): Greeting {
+        return Greeting(counter.incrementAndGet(), String.format(template, name))
+    }
+
+    companion object {
+        private const val template = "Hello, %s!"
+    }
+}
---
+++ 05_FirstSteps/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.erudio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinErudioApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinErudioApplication>(*args)
+}
