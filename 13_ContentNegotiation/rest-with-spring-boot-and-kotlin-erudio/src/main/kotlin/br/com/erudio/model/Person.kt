dd2c2bf9459ab8ba4e25f2a682392ca1a924a554
leandrocgsi <leandrocgsi@gmail.com>
2022-02-09 15:03:47 -0300

    Implementing Content Negotiation with YAML

---
+++ 13_ContentNegotiation/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/model/Person.kt
@@ -0,0 +1,29 @@
+package br.com.erudio.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "person")
+class Person (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(name = "first_name", nullable = false, length = 80)
+    var firstName: String = "",
+
+    @Column(name = "last_name", nullable = false, length = 80)
+    var lastName: String = "",
+
+    @Column(nullable = false, length = 100)
+    var address: String = "",
+
+    @Column(nullable = false, length = 6)
+    var gender: String = ""
+)
