d6dc13fa84b1506571569cad20ec44b3bb77f559
leandrocgsi <leandrocgsi@gmail.com>
2022-02-09 17:16:47 -0300

    Implementing Content Negotiation with YAML

--- 13_ContentNegotiation/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/WebConfig.kt
+++ 13_ContentNegotiation/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/WebConfig.kt
@@ -1,55 +1,51 @@
-package br.com.erudio.config;
+package br.com.erudio.config
 
-import java.util.List;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
+import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer{
-    
-    private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
-    
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new YamlJackson2HttpMessageConverter());
+class WebConfig : WebMvcConfigurer {
+
+    private val MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml")
+
+    override fun extendMessageConverters(converters: MutableList<HttpMessageConverter<*>?>) {
+        converters.add(YamlJackson2HttpMessageConverter())
     }
-    
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer ) {
-        
-        // Via EXTENSION. localhost:8080/person.x-yaml
-        /*
-         * configurer.favorParameter(false) .ignoreAcceptHeader(false)
-         * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-         * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-         */
-        
-        // Via QUERY PARAM. localhost:8080/person?mediaType=xml 
-        
-          configurer.favorParameter(true)
-              .parameterName("mediaType") .ignoreAcceptHeader(true)
-              .useRegisteredExtensionsOnly(false)
-              .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-              MediaType.APPLICATION_JSON) .mediaType("xml",
-              MediaType.APPLICATION_XML);
-         
-        
-        /*configurer
-            .favorParameter(false)
+
+    override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
+
+        // https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
+        // Via EXTENSION. localhost:8080/person.xml DEPRECATED on SpringBoot 2.6
+        /**
+        configurer.favorParameter(false)
+            .ignoreAcceptHeader(false)
+            .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+        */
+
+
+        // Via QUERY PARAM. localhost:8080/person?mediaType=xml
+        /**
+        configurer.favorParameter(true)
+            .parameterName("mediaType").ignoreAcceptHeader(true)
+            .useRegisteredExtensionsOnly(false)
+            .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+        */
+
+        configurer.favorParameter(false)
             .ignoreAcceptHeader(false)
             .useRegisteredExtensionsOnly(false)
             .defaultContentType(MediaType.APPLICATION_JSON)
-            .mediaType("json", MediaType.APPLICATION_JSON)
-            .mediaType("xml", MediaType.APPLICATION_XML)
-            .mediaType("x-yaml", MEDIA_TYPE_YML);
-        */
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
     }
-
 }
--- 13_ContentNegotiation/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/serialization/converter/YamlJackson2HttpMessageConverter.kt
+++ 13_ContentNegotiation/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/serialization/converter/YamlJackson2HttpMessageConverter.kt
@@ -1,16 +1,10 @@
-package br.com.erudio.serialization.converter;
+package br.com.erudio.serialization.converter
 
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-
-public final class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter{
-
-    public YamlJackson2HttpMessageConverter() {
-        super(new YAMLMapper()
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL), MediaType.parseMediaType("application/x-yaml"));
-    }
-
-}
+class YamlJackson2HttpMessageConverter : AbstractJackson2HttpMessageConverter(
+    YAMLMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL), MediaType.parseMediaType("application/x-yaml")
+)
