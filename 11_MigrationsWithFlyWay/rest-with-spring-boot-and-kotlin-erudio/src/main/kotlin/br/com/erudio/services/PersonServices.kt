8b8a1d30e2b62478efdb80031024029685b3927e
leandrocgsi <leandrocgsi@gmail.com>
2022-02-09 22:16:44 -0300

    Adding migrations support with Flyway

--- 11_MigrationsWithFlyWay/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/services/PersonServices.kt
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/services/PersonServices.kt
@@ -1,11 +1,12 @@
 package br.com.erudio.services
 
+import br.com.erudio.data.vo.v1.PersonVO
 import br.com.erudio.exception.ResourceNotFoundException
+import br.com.erudio.mapper.DozerConverter
 import br.com.erudio.model.Person
 import br.com.erudio.repository.PersonRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.util.function.Supplier
 
 @Service
 class PersonServices {
@@ -13,32 +14,34 @@ class PersonServices {
     @Autowired
     private lateinit  var repository: PersonRepository
 
-    fun create(person: Person): Person {
-        return repository.save(person)
+    fun create(person: PersonVO?): PersonVO? {
+        val entity: Person = DozerConverter.parseObject(person, Person::class.java)
+        return DozerConverter.parseObject(repository.save(entity), PersonVO::class.java)
     }
 
-    fun findAll(): List<Person> {
-        return repository.findAll()
+    fun findAll(): List<PersonVO>? {
+        return DozerConverter.parseListObjects(repository.findAll(), PersonVO::class.java)
     }
 
-    fun findById(id: Long): Person {
-        return repository.findById(id)
-            .orElseThrow { ResourceNotFoundException("No records found for this ID") }!!
+    fun findById(id: Long?): PersonVO? {
+        val entity = repository.findById(id!!)
+            .orElseThrow<RuntimeException> { ResourceNotFoundException("No records found for this ID") }
+        return DozerConverter.parseObject(entity, PersonVO::class.java)
     }
 
-    fun update(person: Person): Person {
-        val entity: Person = repository.findById(person.id)
-            .orElseThrow(Supplier { ResourceNotFoundException("No records found for this ID") })
-        entity.firstName = person.firstName
-        entity.lastName = person.lastName
-        entity.address = person.address
-        entity.gender = person.gender
-        return repository.save(entity)
+    fun update(person: PersonVO?): PersonVO? {
+        val entity = repository.findById(person!!.id!!)
+            .orElseThrow<RuntimeException> { ResourceNotFoundException("No records found for this ID") }
+        entity.firstName = person.firstName!!
+        entity.lastName = person.lastName!!
+        entity.address = person.address!!
+        entity.gender = person.gender!!
+        return DozerConverter.parseObject(repository.save(entity), PersonVO::class.java)
     }
 
-    fun delete(id: Long) {
-        val entity: Person = repository.findById(id)
-            .orElseThrow(Supplier { ResourceNotFoundException("No records found for this ID") })
+    fun delete(id: Long?) {
+        val entity = repository.findById(id!!)
+            .orElseThrow<RuntimeException> { ResourceNotFoundException("No records found for this ID") }
         repository.delete(entity)
     }
 }
