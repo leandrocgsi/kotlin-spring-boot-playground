20a79959aeec0408d8a174b391eeb53cc0995893
leandrocgsi <leandrocgsi@gmail.com>
2022-02-13 00:08:48 -0300

    Working with PATCH Method

---
+++ 18_WorkingWithPATCHMethod/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/controller/withxml/AuthControllerXmlTest.kt
@@ -0,0 +1,71 @@
+package br.com.erudio.integrationtests.controller.withxml
+
+import io.restassured.RestAssured.given
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.boot.test.context.SpringBootTest
+
+import br.com.erudio.configs.TestsConfig
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest
+import br.com.erudio.integrationtests.vo.AccountCredentialsVO
+import br.com.erudio.integrationtests.vo.TokenVO
+import org.junit.jupiter.api.TestInstance
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AuthControllerXmlTest : AbstractIntegrationTest() {
+
+    private var tokenVO: TokenVO? = null
+
+    @Test
+    @Order(1)
+    fun testSignin() {
+        val user = AccountCredentialsVO()
+        user.username = "leandro"
+        user.password = "admin123"
+
+        tokenVO = given()
+            .basePath("/auth/signin")
+            .port(TestsConfig.SERVER_PORT)
+            .contentType(TestsConfig.CONTENT_TYPE_XML)
+            .body(user)
+            .`when`()
+            .post()
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .`as`(TokenVO::class.java)
+
+        assertNotNull(tokenVO!!.accessToken)
+        assertNotNull(tokenVO!!.refreshToken)
+    }
+
+    @Test
+    @Order(2)
+    fun testRefresh() {
+        val newTokenVO = given()
+            .basePath("/auth/refresh")
+            .port(TestsConfig.SERVER_PORT)
+            .contentType(TestsConfig.CONTENT_TYPE_XML)
+            .pathParam("username", tokenVO!!.username)
+            .header(TestsConfig.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenVO!!.refreshToken)
+            .`when`()
+            .put("{username}")
+            .then()
+            .statusCode(200)
+            .extract()
+            .body()
+            .`as`(TokenVO::class.java)
+
+        assertNotNull(tokenVO!!.accessToken)
+        assertNotNull(tokenVO!!.refreshToken)
+    }
+}
