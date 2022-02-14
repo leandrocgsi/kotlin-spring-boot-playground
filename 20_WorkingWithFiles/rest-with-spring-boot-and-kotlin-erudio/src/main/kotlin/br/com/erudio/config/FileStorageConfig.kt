37896bf96dca766e3f18867189b7ce99a1444109
leandrocgsi <leandrocgsi@gmail.com>
2022-02-14 17:42:33 -0300

    Working with files

---
+++ 20_WorkingWithFiles/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/FileStorageConfig.kt
@@ -0,0 +1,11 @@
+package br.com.erudio.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "file")
+class FileStorageConfig {
+
+    var uploadDir: String? = null
+}
---
+++ 20_WorkingWithFiles/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/exception/FileStorageException.kt
@@ -0,0 +1,12 @@
+package br.com.erudio.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+class FileStorageException : RuntimeException {
+
+    constructor(exception: String?) : super(exception)
+    constructor(exception: String?, cause: Throwable?) : super(exception, cause)
+
+}
