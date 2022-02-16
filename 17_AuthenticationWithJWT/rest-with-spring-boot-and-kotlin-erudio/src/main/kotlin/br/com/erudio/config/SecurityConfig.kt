5708a717b22e3a3a8247f00ee0b0b77c2df91efa
leandrocgsi <leandrocgsi@gmail.com>
2022-02-15 21:27:13 -0300

    Implementing Authenticatin With JWT

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/SecurityConfig.kt
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/SecurityConfig.kt
@@ -1,21 +1,17 @@
 package br.com.erudio.config
 
-import java.util.HashMap;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-
-import br.com.erudio.security.jwt.JwtConfigurer;
-import br.com.erudio.security.jwt.JwtTokenProvider;
-
+import br.com.erudio.security.jwt.JwtConfigurer
+import br.com.erudio.security.jwt.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
 
 
 @Configuration
@@ -40,7 +36,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
     }
 
     @Throws(Exception::class)
-    protected override fun configure(http: HttpSecurity) {
+    override fun configure(http: HttpSecurity) {
         http
             .httpBasic().disable()
             .csrf().disable()
