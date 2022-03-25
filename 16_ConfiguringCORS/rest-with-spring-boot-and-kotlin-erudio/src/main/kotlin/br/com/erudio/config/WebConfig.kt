039d91daeb03e39af46c10927de001136aff2458
leandrocgsi <leandrocgsi@gmail.com>
2022-03-25 09:57:50 -0300

    Configuring Cross-Origin Resource Sharing (CORS)

--- 16_ConfiguringCORS/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/WebConfig.kt
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/config/WebConfig.kt
@@ -18,21 +18,6 @@ class WebConfig : WebMvcConfigurer{
 
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
 
-
-        // https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
-        // Via EXTENSION. http://localhost:8080/api/person/v1.xml DEPRECATED on SpringBoot 2.6
-
-        // Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
-        /*
-        configurer.favorParameter(true)
-            .parameterName("mediaType")
-            .ignoreAcceptHeader(true)
-            .useRegisteredExtensionsOnly(false)
-            .defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML)
-        */
-
         configurer.favorParameter(false)
             .ignoreAcceptHeader(false)
             .useRegisteredExtensionsOnly(false)
--- 16_ConfiguringCORS/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
@@ -1,6 +1,6 @@
 package br.com.erudio.integrationtests
 
-object ConfigsTest {
+object TestConfigs {
     const val SERVER_PORT = 8888
     const val HEADER_PARAM_ORIGIN = "Origin"
     const val CONTENT_TYPE_JSON = "application/json"
