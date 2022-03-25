e7b4d349d022be98f9fe47406047a92696803757
leandrocgsi <leandrocgsi@gmail.com>
2022-03-25 10:01:04 -0300

    Implementing Authenticatin With JWT

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
@@ -0,0 +1,7 @@
+package br.com.erudio.integrationtests
+
+object TestConfigs {
+    const val SERVER_PORT = 8888
+    const val HEADER_PARAM_ORIGIN = "Origin"
+    const val CONTENT_TYPE_JSON = "application/json"
+}
