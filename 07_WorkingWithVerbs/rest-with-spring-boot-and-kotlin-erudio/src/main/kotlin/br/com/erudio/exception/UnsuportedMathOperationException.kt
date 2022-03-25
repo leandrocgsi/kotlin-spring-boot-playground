5c666e22990ce6f493df153d67213b1d24a509f5
leandrocgsi <leandrocgsi@gmail.com>
2022-03-25 15:35:59 -0300

    Working with four HTTP verbs

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/exception/UnsuportedMathOperationException.kt
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/exception/UnsuportedMathOperationException.kt
@@ -4,8 +4,4 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-class UnsuportedMathOperationException(exception: String?) : RuntimeException(exception) {
-    companion object {
-        private const val serialVersionUID = 1L
-    }
-}
+class UnsupportedMathOperationException(exception: String?) : RuntimeException(exception)
