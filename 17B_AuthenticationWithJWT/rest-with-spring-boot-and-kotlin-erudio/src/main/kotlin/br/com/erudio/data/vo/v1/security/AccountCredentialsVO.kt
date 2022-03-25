2178faf525e7b04117469ad7d784f879a34344de
leandrocgsi <leandrocgsi@gmail.com>
2022-03-25 20:11:29 -0300

    Implementing Authentication with JWT - Check Implementations with Integration Tests

---
+++ 17B_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/data/vo/v1/security/AccountCredentialsVO.kt
@@ -0,0 +1,6 @@
+package br.com.erudio.data.vo.v1.security
+
+data class AccountCredentialsVO (
+    var username: String? = null,
+    var password: String? = null
+)
---
+++ 17B_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/model/Permission.kt
@@ -0,0 +1,19 @@
+package br.com.erudio.model
+
+import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
+
+@Entity
+@Table(name = "permission")
+class Permission : GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null
+
+    @Column(name = "description")
+    var description: String? = null
+
+    override fun getAuthority() = description!!
+}
