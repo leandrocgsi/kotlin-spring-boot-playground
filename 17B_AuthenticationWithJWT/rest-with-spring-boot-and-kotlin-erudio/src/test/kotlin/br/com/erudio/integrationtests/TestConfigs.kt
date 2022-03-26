ac6d7feb753e730cf440ae903f6ba58173ea71e8
leandrocgsi <leandrocgsi@gmail.com>
2022-03-26 11:07:43 -0300

    Implementing Authentication with JWT - Check Implementations with Integration Tests

--- 17B_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
+++ 17B_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/TestConfigs.kt
@@ -3,9 +3,11 @@ package br.com.erudio.integrationtests
 object TestConfigs {
     const val SERVER_PORT = 8888
 
+    const val HEADER_PARAM_AUTHORIZATION = "Authorization"
+
     const val CONTENT_TYPE_JSON = "application/json"
     const val CONTENT_TYPE_XML = "application/xml"
-    const val CONTENT_TYPE_YAML = "application/x-yaml"
+    const val CONTENT_TYPE_YML = "application/x-yaml"
 
     const val HEADER_PARAM_ORIGIN = "Origin"
 
--- 17B_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/vo/PersonVO.kt
+++ 17B_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/vo/PersonVO.kt
@@ -1,5 +1,12 @@
 package br.com.erudio.integrationtests.vo
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import jakarta.xml.bind.annotation.XmlRootElement
+
+@XmlRootElement
+@JsonIgnoreProperties
+@JsonPropertyOrder( "id", "firstName", "lastName", "address", "gender" )
 data class PersonVO (
 
     var id: Long = 0,
