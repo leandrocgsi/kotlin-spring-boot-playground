433913a6ba2b4230d9ad46246f9f6843609e8097
leandrocgsi <leandrocgsi@gmail.com>
2022-03-31 12:59:44 -0300

    Implementing Upload and Download Files

---
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/data/vo/v1/UploadFileResponseVO.kt
@@ -0,0 +1,8 @@
+package br.com.erudio.data.vo.v1
+
+class UploadFileResponseVO (
+    var fileName: String = "",
+    var fileDownloadUri: String = "",
+    var fileType: String = "",
+    var size: Long = 0
+)
