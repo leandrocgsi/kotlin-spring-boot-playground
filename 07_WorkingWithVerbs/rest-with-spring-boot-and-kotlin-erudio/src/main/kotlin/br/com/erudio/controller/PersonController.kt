13627d81224871fd72a3d330b1a0e3900f13cd27
leandrocgsi <leandrocgsi@gmail.com>
2023-01-12 17:21:39 -0300

    Working with four HTTP Main Verbs

--- 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
+++ 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/controller/PersonController.kt
@@ -1,17 +1,27 @@
 package br.com.erudio.controller
 
+import br.com.erudio.converters.NumberConverter
+import br.com.erudio.exceptions.UnsupportedMathOperationException
+import br.com.erudio.math.SimpleMath
 import br.com.erudio.model.Person
-import br.com.erudio.services.PersonServices
+import br.com.erudio.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
 
 @RestController
 @RequestMapping("/person")
 class PersonController {
 
+
     @Autowired
-    private lateinit  var service: PersonServices
+    private lateinit var service: PersonService
+    // var service: PersonService = PersonService()
 
     @RequestMapping(method = [RequestMethod.GET],
         produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -19,31 +29,32 @@ class PersonController {
         return service.findAll()
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findById(@PathVariable("id") id: String): Person {
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET],
+                    produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findById(@PathVariable(value="id") id: Long): Person {
         return service.findById(id)
     }
 
-    @RequestMapping(
-        method = [RequestMethod.POST],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
+    @RequestMapping(method = [RequestMethod.POST],
+                    consumes = [MediaType.APPLICATION_JSON_VALUE],
+                    produces = [MediaType.APPLICATION_JSON_VALUE])
     fun create(@RequestBody person: Person): Person {
         return service.create(person)
     }
 
-@RequestMapping(
-    method = [RequestMethod.PUT],
-    consumes = [MediaType.APPLICATION_JSON_VALUE],
-    produces = [MediaType.APPLICATION_JSON_VALUE]
-)
-fun update(@RequestBody person: Person): Person {
-    return service.update(person)
-}
+    @RequestMapping(method = [RequestMethod.PUT],
+                    consumes = [MediaType.APPLICATION_JSON_VALUE],
+                    produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun update(@RequestBody person: Person): Person {
+        return service.update(person)
+    }
 
-@RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE])
-fun delete(@PathVariable("id") id: String) {
-    service.delete(id)
-}
+    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE],
+        produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun delete(@PathVariable(value="id") id: Long) {
+        service.delete(id)
+    }
+
+
+
 }
--- 07_WorkingWithVerbs/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/exception/UnsupportedMathOperationException.kt
+++
@@ -1,7 +0,0 @@
-package br.com.erudio.exception
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-class UnsupportedMathOperationException(exception: String?) : RuntimeException(exception)
