29e1406a0dc57e89c0c1f8fbb4262e4608d73564
leandrocgsi <leandrocgsi@gmail.com>
2023-01-12 17:22:08 -0300

    Connecting to MySQL with Spring Data, JPA and Hibernate

--- 08_ConnectingToMySQL/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/converters/NumberConverter.kt
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/converters/NumberConverter.kt
@@ -1,15 +1,16 @@
-package br.com.erudio.request.converters
+package br.com.erudio.converters
 
 object NumberConverter {
 
-    fun covertToDouble(strNumber: String?): Double {
-        if (strNumber == null) return 0.0
+    fun convertToDouble(strNumber: String?): Double {
+        if (strNumber.isNullOrBlank()) return 0.0
+        // BR 10,20 US 10.20
         val number = strNumber.replace(",".toRegex(), ".")
         return if (isNumeric(number)) number.toDouble() else 0.0
     }
 
     fun isNumeric(strNumber: String?): Boolean {
-        if (strNumber == null) return false
+        if (strNumber.isNullOrBlank()) return false;
         val number = strNumber.replace(",".toRegex(), ".")
         return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
     }
