8d574ad7ac95db31b218bdf2200d8f9ef80ecb73
leandrocgsi <leandrocgsi@gmail.com>
2023-01-12 17:25:01 -0300

    Implementing Authentication With Spring Security and JWT

--- 17B_AuthenticationWithJWT/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/util/MediaType.kt
+++
@@ -1,8 +0,0 @@
-package br.com.erudio.util
-
-object MediaType {
-
-    const val APPLICATION_JSON = "application/json"
-    const val APPLICATION_XML = "application/xml"
-    const val APPLICATION_YML = "application/x-yaml"
-}
