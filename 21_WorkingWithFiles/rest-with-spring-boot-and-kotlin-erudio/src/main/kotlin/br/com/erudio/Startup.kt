8ba0312d590e742647271803629d2cd161960ebe
leandrocgsi <leandrocgsi@gmail.com>
2023-01-13 20:07:58 -0300

    Implementing Upload and Download Files

--- 21_WorkingWithFiles/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/Startup.kt
+++ 21_WorkingWithFiles/rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/Startup.kt
@@ -14,12 +14,4 @@ class Startup
 
 fun main(args: Array<String>) {
 	runApplication<Startup>(*args)
-
-	val encoders: MutableMap<String, PasswordEncoder> = HashMap()
-	encoders["pbkdf2"] = Pbkdf2PasswordEncoder()
-	val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
-	passwordEncoder.setDefaultPasswordEncoderForMatches(Pbkdf2PasswordEncoder())
-
-	val result = passwordEncoder.encode("1234")
-	println("My hash $result")
 }
